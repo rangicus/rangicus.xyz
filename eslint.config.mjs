baea14f5f3f4920f7e34d8a86760e122d8d2eb92
Xavier Horn <xavierhorn00@gmail.com>
2025-03-08 15:19:28 -0500

    Initial.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// Imports
+
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import stylistic from "@stylistic/eslint-plugin";
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// Compat
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// ESLint Config
 
-export default eslintConfig;
+export default defineConfig([
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	eslintPluginUnicorn.configs.recommended,
+
+	// Stylistic Rules
+	{
+		plugins: { "@stylistic": stylistic },
+		rules: {
+			indent: [`warn`, `tab`],
+		}
+	},
+]);
